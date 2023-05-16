074dee4599ea63840bd3986e5ebdf804ebbdfda5
ishanrathore04 <92152269+ishanrathore04@users.noreply.github.com>
2023-05-16 23:36:57 +0530

    Create main.java

---
+++ LE3/LE3_1/main.java
@@ -0,0 +1,26 @@
+public class Main
+{
+	public static void main(String[] args) {
+		Arjun a=new Arjun();
+		a.show();
+		a.fight();
+        a.obey();
+        a.kind();
+        Bheem b=new Bheem();
+        b.show();
+		b.fight();
+        b.obey();
+        b.kind();
+        Duryodhan d=new Duryodhan();
+        d.show();
+		d.fight();
+        d.obey();
+        d.kind();
+        Vikarn v=new Vikarn();
+        v.show();
+		v.fight();
+        v.obey();
+        v.kind();
+        
+	}
+}
